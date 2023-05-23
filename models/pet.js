f22182c73a3911867261a5d2760ad9d008d5f1db
VitaliyMeshechek <meshechek.vitaliy@ukr.net>
2023-05-23 04:32:42 +0300

    add petSchema

---
+++ models/pet.js
@@ -0,0 +1,66 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const nameFormat = /^([A-Za-z\-\']{1,20})|([А-Яа-я\-\']{1,20})$/;
+const dateFormat =
+  /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+// eslint-disable-next-line no-useless-escape
+const textFormat = /^[A-Za-z0-9+_.-]+@(.+)$/;
+
+const { handleMongooseError } = require("..//helpers");
+
+const petSchema = new Schema(
+  {
+    name: {
+      type: String,
+      match: nameFormat,
+      required: true,
+    },
+    birthday: {
+      type: String,
+      match: dateFormat,
+      required: true,
+    },
+    breed: {
+      type: String,
+      match: nameFormat,
+      required: true,
+    },
+    avatarURL: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    comments: {
+      type: String,
+      match: textFormat,
+      default: "",
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+petSchema.post("save", handleSaveErrors);
+
+const petAddSchema = Joi.object({
+  name: Joi.string().pattern(nameFormat).required(),
+  birthday: Joi.string().pattern(dateFormat).required(),
+  breed: Joi.string().pattern(nameFormat).required(),
+  comments: Joi.string().min(10).max(120).pattern(textFormat).required(),
+  avatarURL: Joi.string().optional(),
+});
+
+const petSchemas = {
+  petAddSchema,
+};
+
+const Pet = model("pet", petSchema);
+
+module.exports = {
+  Pet,
+  petSchemas,
+};
