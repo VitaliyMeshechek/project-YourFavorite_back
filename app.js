ac776a06d8e85f27ad0188392c59990c950d44b6
VitaliyMeshechek <meshechek.vitaliy@ukr.net>
2023-06-02 00:05:14 +0300

    111

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = "Server error" } = err;
-//   res.status(status).json({ message: err.message });
-// });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message: err.message });
+});
 
 module.exports = app;
